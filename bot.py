d6cba9debbd65e96501aae5282fa3bdcfe4cd3bc
Denis <45421827+Poehavshy@users.noreply.github.com>
2020-04-22 22:14:43 +0400

    add photo

--- bot.py
+++ bot.py
@@ -21,6 +21,33 @@ def main():
     updater.start_polling(timeout=123)
 
     updater.idle()
+def photo(update, context):
+    con = pymysql.connect(config.DB_SERVER, config.DB_USER, config.DB_PASSWORD, config.DB_DATABASE)
+    with con:
+        cur = con.cursor()
+        cur.execute(f"SELECT `status` FROM `Users` WHERE `telegram_id` = '{update.message.from_user.id}';")
+        if cur.rowcount == 0:
+            return
+        res = cur.fetchall()
+        status = res[0][0]
+        if status == const.STATUS[2] or status == const.STATUS[7]:
+            update.message.reply_text(text="Загружаю изображение...", reply_markup=None)
+            file_id = update.message.document.file_id
+            file = context.bot.getFile(file_id)
+            file_name = f"{update.message.from_user.id}_{datetime.datetime.now()}.png"
+
+        if status == const.STATUS[2]:
+            file.download(f"photo/encrypt/before/{file_name}")
+            cur.execute(f"UPDATE `Users` set `status`='{const.STATUS[1]}', `image`='{file_name}' WHERE `telegram_id`='{update.message.from_user.id}';")
+            con.commit()
+            reply_markup = get_keyboard(update.message.from_user.id, cur)
+            update.message.reply_text(text="Выбери, что установить.", reply_markup=reply_markup)
+        if status == const.STATUS[7]:
+            file.download(f"photo/decrypt/before/{file_name}")
+            cur.execute(f"UPDATE `Users` set `status`='{const.STATUS[6]}', `image`='{file_name}' WHERE `telegram_id`='{update.message.from_user.id}';")
+            con.commit()
+            reply_markup = get_keyboard(update.message.from_user.id, cur)
+            update.message.reply_text(text="Выбери, что установить.", reply_markup=reply_markup)
 
 
 if __name__ == "__main__":
