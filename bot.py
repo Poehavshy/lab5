b2f1fd4719f6d54b409cff6a26a166dbd079483d
Denis <45421827+Poehavshy@users.noreply.github.com>
2020-04-22 22:23:14 +0400

    add start

--- bot.py
+++ bot.py
@@ -8,6 +8,20 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram import ReplyKeyboardMarkup
 
+def start(update, context):
+    con = pymysql.connect(config.DB_SERVER, config.DB_USER, config.DB_PASSWORD, config.DB_DATABASE)
+    with con:
+        cur = con.cursor()
+        cur.execute(f"SELECT * FROM `Users` WHERE `telegram_id` = '{update.message.from_user.id}';")
+        if cur.rowcount == 0:
+            cur.execute(f"INSERT INTO `Users` SET `telegram_id`='{update.message.from_user.id}',`name`='{update.message.from_user.first_name}', `status`='{const.STATUS[0]}', `image`=NULL, `text`=NULL, `crypto_key`=NULL;")
+        else:
+            cur.execute(f"UPDATE `Users` SET `name`='{update.message.from_user.first_name}', `status`='{const.STATUS[0]}', `image`=NULL, `text`=NULL, `crypto_key`=NULL WHERE `telegram_id`='{update.message.from_user.id}';")
+        con.commit()
+        reply_markup = ReplyKeyboardMarkup([['Шифрование', 'Дешифрование']], resize_keyboard=True, one_time_keyboard=True)
+        update.message.reply_text(text="Приветствую, я CryptoBot, умею шифровать сообщение в картинку.", reply_markup=reply_markup)
+
+
 
 def main():
     updater = Updater(token=config.TOKEN, use_context=True, request_kwargs=config.REQUEST_KWARGS)
