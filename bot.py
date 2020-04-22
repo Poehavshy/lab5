a8e4c6577fd086f27ea50df7c62476d6e30d1667
Denis <45421827+Poehavshy@users.noreply.github.com>
2020-04-22 22:21:21 +0400

    add keyboard

--- bot.py
+++ bot.py
@@ -9,6 +9,31 @@ from telegram.ext import MessageHandler
 from telegram import ReplyKeyboardMarkup
 
 
+def get_keyboard(user_id, cur):
+    cur.execute(f"SELECT `status`, `image`, `text`, `crypto_key` FROM `Users` WHERE `telegram_id` = '{user_id}';")
+    res = cur.fetchall()[0]
+    keyboard = [[]]
+    reply_markup = None
+    if res[0] == const.STATUS[1]:
+        keyboard[0].append(('' if res[1] is None else u'\U00002705') + "Изображение")
+        keyboard[0].append(('' if res[2] is None else u'\U00002705') + "Текст")
+        keyboard[0].append(('' if res[3] is None else u'\U00002705') + "Ключ")
+        if res[1] is not None and res[2] is not None and res[3] is not None:
+            keyboard.append([])
+            keyboard[1].append("Завершить")
+        reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
+    if res[0] == const.STATUS[6]:
+        keyboard[0].append(('' if res[1] is None else u'\U00002705') + "Изображение")
+        keyboard[0].append(('' if res[3] is None else u'\U00002705') + "Ключ")
+        if res[1] is not None and res[3] is not None:
+            keyboard.append([])
+            keyboard[1].append("Завершить")
+        reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
+
+    return reply_markup
+
+
+
 def main():
     updater = Updater(token=config.TOKEN, use_context=True, request_kwargs=config.REQUEST_KWARGS)
     dp = updater.dispatcher
